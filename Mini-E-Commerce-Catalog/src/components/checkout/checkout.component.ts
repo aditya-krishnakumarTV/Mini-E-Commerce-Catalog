6e60b7c7a08e71f74b47755a2d9b1ad3498c5245
aditya_kk <adityakrishnakumar12@gmail.com>
2025-06-18 12:59:56 +0530

    Project components, services and models

---
+++ Mini-E-Commerce-Catalog/src/components/checkout/checkout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-checkout',
+    templateUrl: './checkout.component.html',
+    styleUrl: './checkout.component.css',
+    standalone: true
+})
+export class CheckoutComponent { }
